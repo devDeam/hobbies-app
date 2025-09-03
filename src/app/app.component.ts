437adfa908e0ca4a86807db421f887130918040d
Daniel Almanza <daniel.deam123@gmail.com>
2025-09-03 00:13:21 -0500

    Se modifica estructura del index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgbCollapseModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, NgbCollapseModule, NgbAccordionModule],
+  imports: [RouterOutlet, CommonModule, NgbCollapseModule, NgbAccordionModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
