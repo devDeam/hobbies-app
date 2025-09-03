0b83c4413e92d7f9b28ff7b65636335c1f6cf82b
Daniel Almanza <daniel.deam123@gmail.com>
2025-09-03 00:02:30 -0500

    Se agrega un acordean para ver las series

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, NgbCollapseModule],
+  imports: [RouterOutlet, CommonModule, NgbCollapseModule, NgbAccordionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
